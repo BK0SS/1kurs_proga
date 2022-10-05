3c4ac9cff73aa62699ad51e7cc1da37662097caf
Bodya <Bodya@HOME-PC>
2022-10-05 20:24:48 +0500

    task 1 colour change

---
+++ Task_01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш стих");
+            Random rnd = new Random();
+
+            string input = Console.ReadLine();
+            String[] strings = input.Split(' ', '-', ';', ',');
+
+            foreach (string i in strings)
+            {
+                int a = rnd.Next(1, 3);
+                if (a == 1) { Console.ForegroundColor = ConsoleColor.Yellow; Console.Write(i + ' '); }
+                if (a == 2) { Console.ForegroundColor = ConsoleColor.Red; Console.Write(i + ' '); }
+                if (a == 3) { Console.ForegroundColor = ConsoleColor.Blue; Console.Write(i + ' '); }
+
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(input);
+            while (true) ;
+
+
+        }
+    }
+}
