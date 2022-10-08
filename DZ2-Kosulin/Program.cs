b61f7090ca4f6dfc3f2e76011cbedf637cdad49c
Bodya <Bodya@HOME-PC>
2022-10-08 15:59:49 +0500

    Задание-2 Вычисление Длины медианы треугольника

---
+++ DZ2-Kosulin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2_Kosulin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dlina_1 = double.Parse(Console.ReadLine());
+            double dlina_2 = double.Parse(Console.ReadLine());    
+            double dlina_3 = double.Parse(Console.ReadLine());
+            //Это медиана для 3 стороны то есть для dlina_3
+            Console.WriteLine(Math.Sqrt(2*Math.Pow(dlina_1,2)+ 2*Math.Pow(dlina_2,2)+Math.Pow(dlina_3,2) /4));
+            while (true) ;
+        }
+    }
+}
