fdcff829db601da8ec0b3d6c96314d1c932c4dde
Bodya <Bodya@HOME-PC>
2022-10-17 15:08:06 +0500

    Задание 5- метод вычисляющий значение функций

---
+++ zadanie5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie5
+{
+    internal class Program
+    {
+        static float Untic_x(float x, float a, float b) 
+        {
+            return x = (float)Math.Pow((float)Math.E, -Math.Sqrt(a + Math.Pow(b, (float)Math.PI)));
+        }
+        static void Main(string[] args)
+        {
+            float y = 3;
+            Console.WriteLine( Untic_x(y,2,3)+ Untic_x(y,3,5)+ Untic_x(y,5,7));
+            Console.ReadKey();
+        }
+    }
+}
