398fdb0be30d1e3d3d85f316aa4394d4db813924
BK0SS <114229881+BK0SS@users.noreply.github.com>
2022-10-17 14:09:41 +0500

    Update Program.cs

--- DZ-3Kosulin/Program.cs
+++ DZ-3Kosulin/Program.cs
@@ -14,7 +14,7 @@ namespace DZ_3Kosulin
             float x= float.Parse(Console.ReadLine());
             Console.WriteLine("Y =" + 1+Math.Sqrt(Math.Pow(x,2)-1/Math.Pow(x,2)+1));
 
-            while (true) ;
+            Console.ReadKey();
 
             
         }
