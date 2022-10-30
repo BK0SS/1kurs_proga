361083a922968b65c380b2df70475cb561f37edd
Bodya <Bodya@HOME-PC>
2022-10-30 12:55:36 +0500

    Задание 6.2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "клавиатура";
+            string viara = a[3].ToString() + a[4].ToString() + a[2].ToString() + a[8].ToString() + a[2].ToString();
+            string ruka = a[8].ToString() + a[7].ToString() + a[0].ToString() + a[2].ToString();
+            Console.WriteLine(viara + " " + ruka);
+            Console.ReadKey();
+        }
+    }
+}
