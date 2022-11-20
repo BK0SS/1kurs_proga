59a6bbc76ce71208c498bc6574f72dfb76b84e63
Bodya <Bodya@HOME-PC>
2022-11-20 11:31:29 +0500

    Задание 8 функция

---
+++ Padanie8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padanie8
+{
+    internal class Program
+    {
+        public static void xfunc(double x)
+        {
+            if (x < -1)
+            {
+                Console.WriteLine(1);
+            }
+            else if (x > 1)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(-x);
+            }
+        }
+        static void Main(string[] args)
+        {
+            xfunc(1.2);
+            xfunc(-2);
+            xfunc(0.5);
+            Console.ReadKey();
+        }
+    }
+}
