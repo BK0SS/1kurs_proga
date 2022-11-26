a935d76ea486548946fc2cb473b1777384a4dccb
BK0SS <114229881+BK0SS@users.noreply.github.com>
2022-11-26 13:21:02 +0500

    Update Program.cs
    Задание 2 исправлено

--- DZ2-Kosulin/Program.cs
+++ DZ2-Kosulin/Program.cs
@@ -11,10 +11,13 @@ namespace DZ2_Kosulin
         static void Main(string[] args)
         {
             double dlina_1 = double.Parse(Console.ReadLine());
-            double dlina_2 = double.Parse(Console.ReadLine());    
-            double dlina_3 = double.Parse(Console.ReadLine());
-            //Это медиана для 3 стороны то есть для dlina_3
-            Console.WriteLine(Math.Sqrt(2*Math.Pow(dlina_1,2)+ 2*Math.Pow(dlina_2,2)+Math.Pow(dlina_3,2) /4));
+            double dlina_2 = double.Parse(Console.ReadLine());
+            double gipotin = Math.Sqrt(Math.Pow(dlina_1,2) + Math.Pow(dlina_2,2));
+            double meiana1, mediana2, mediana3;
+            meiana1 = Math.Sqrt(2 * Math.Pow(dlina_1, 2) + 2 * Math.Pow(dlina_2, 2) + Math.Pow(gipotin, 2) / 4);
+            mediana2 =  Math.Sqrt(2 * Math.Pow(dlina_2, 2) + 2 * Math.Pow(gipotin, 2) + Math.Pow(dlina_1, 2) / 4);
+            mediana3 = Math.Sqrt(2 * Math.Pow(dlina_1, 2) + 2 * Math.Pow(gipotin, 2) + Math.Pow(dlina_2, 2) / 4);
+            Console.ReadKey();
             Console.ReadKey() ;
         }
     }
