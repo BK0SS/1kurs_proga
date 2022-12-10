8230318157214ab0f9b719a5c8f3fa7a0ec7f452
Bodya <Bodya@HOME-PC>
2022-12-10 18:06:27 +0500

    Задание 9(Номер 1)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static int function(int n)
+        {
+            int x= (int) Math.Pow(n,2);
+
+            for (int i = 1; i <= n; i++)
+            {
+                x += (int)Math.Pow(n + i, 2);
+            }
+            x += (int)Math.Pow(2 * n, 2);
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            int y = function(int.Parse(Console.ReadLine()));
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
