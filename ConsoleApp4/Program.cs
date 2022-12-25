3e44da683ca6e98ea2f56cb2df28c88afa466cf4
Богдан <114229881+BK0SS@users.noreply.github.com>
2022-12-25 17:23:01 +0500

    Задание 9.4 переделано
    Сделал только для тех случаев, когда мы берем гирю  5, 2, 1кг N-ое количество раз. Не понял как сделать через цикл чтоб были все вариаций

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,75 +8,29 @@ namespace ConsoleApp4
 {
     internal class Program
     {
-        static string Girya(int m)
+        static int Dumbell(int kg)
         {
-            int g1 = 1;
-            int g2 = 2;
-            int g3 = 5;
-            int rznt = 0;
-           
-                if(g1 + g2 == m)
-                {
-                    Console.WriteLine($"этот способ {g1} + {g2} подошел");
-                    rznt++;
-                }
-                if(g3-g2 == m)
-                {
-                Console.WriteLine($"этот способ {g3} - {g2} подошел");
-                rznt++;
-                }
-                if (g3 + g1 == m)
-                {
-                    Console.WriteLine($"этот способ {g3} + {g1} подошел");
-                    rznt++;
-
-                }
-                if (g2 + g3 == m)
-                {
-                    Console.WriteLine($"этот способ {g2} + {g3} подошел");
-                    rznt++;
-
-                }
-                if (g1==m||g2 == m || g3 == m)
-                {
-                    Console.WriteLine("вес равен одной из гирь");
-                    rznt++;
-
-                }
-                if (g1 + g2 + g3==m)
-                {
-                    Console.WriteLine($"этот способ {g1} + {g2} + {g3} подошел");
-                    rznt++;
-
-                }
-                if (g3 - g1 + g2 == m)
-                {
-                    Console.WriteLine($"этот способ {g3} - {g1} + {g2} подошел");
-                    rznt++;
-
-                }
-                if (g3 - g1 == m)
-                {
-                    Console.WriteLine($"этот способ {g3} - {g1}  подошел");
-                    rznt++;
-
-                }
-                if (g3 - g2 + g1 == m)
-                {
-                    Console.WriteLine($"этот способ {g3} - {g1}+ {g1}  подошел");
-                    rznt++;
-
-                }
-
-            
-            return $"всего {rznt} способов";
-
+            string answer = "";
+            for(int i = 1; i < kg; i++)
+            {  
+                    if (kg % i == 0) 
+                    {
+                        if (i == 1 | i == 2 | i == 5)
+                        {
+                            answer += $"{kg} = {kg / i} гирь по {i} кг\n";
+                        }
+                    }         
+            }
+            Console.WriteLine(answer);
+            return 0;
         }
-        
+
         static void Main(string[] args)
         {
-            int m = int.Parse(Console.ReadLine());
-            Console.WriteLine(Girya(m));
+            Console.WriteLine("Введите вес гири");
+            
+            int number = Dumbell(int.Parse(Console.ReadLine()));
+     
             Console.ReadKey();
         }
     }
