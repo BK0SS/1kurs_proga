613498e40272f01a7b0ae3b678b978dd57395b8a
Богдан <114229881+BK0SS@users.noreply.github.com>
2022-12-25 13:56:48 +0500

    Исправил задание 9.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,22 +8,25 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static int function(int n)
+         static void Main(string[] args)
         {
-            int x= (int) Math.Pow(n,2);
+            Console.WriteLine($"Ввод числа n");
+
+            int n = DoubleSquereFunction(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Результат функций равен = {n}");
+            Console.ReadKey();
+        }
+        static int DoubleSquereFunction(int n)
+        {
+            int x = (int)Math.Pow(n, 2);
 
             for (int i = 1; i <= n; i++)
             {
-                x += (int)Math.Pow(n + i, 2);
+                x += (n + i) * (n + i);
             }
-            x += (int)Math.Pow(2 * n, 2);
+            
             return x;
-        }
-        static void Main(string[] args)
-        {
-            int y = function(int.Parse(Console.ReadLine()));
-            Console.WriteLine(y);
-            Console.ReadKey();
         }
     }
 }
