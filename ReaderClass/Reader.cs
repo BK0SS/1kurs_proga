719fa6ba40c25039c3110a3649847a1acf45ea11
Bodya <Bodya@HOME-PC>
2023-03-11 18:27:08 +0500

    Задание11

---
+++ ReaderClass/Reader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderClass
+{
+    public class Reader
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public DateTime IssueDate;
+        public readonly int LTicketNumber;
+        public int Period;
+        public readonly DateTime ReturnDate;
+        public decimal Pledge;
+
+        public Reader(string Name, string Surname, string IssueDate, int LTicketNumber, int Period, decimal Pledge)
+        {
+            this.Name = Name;
+            this.Surname = Surname;
+            this.IssueDate = DateTime.Parse(IssueDate);
+            this.LTicketNumber = LTicketNumber;
+            this.Period = Period;
+            ReturnDate = DateTime.Parse(IssueDate).AddDays(Period);
+            this.Pledge = Pledge;
+        }
+
+        public string GetInfo()
+        {
+
+            return $"{Name} {Surname}, читательский билет: {LTicketNumber}.Дата выдачи {IssueDate:d}, период выдачи:{Period}\n" +
+                $"Дата возврата:{ReturnDate:d}. Залог: {Pledge:c}";
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine(GetInfo());
+        }
+    }
+}
